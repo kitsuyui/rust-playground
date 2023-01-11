46f6ec87e067bcda6a911fa3b99cfd3a648ae3bd
kitsuyui <kitsuyui@kitsuyui.com>
2023-01-11 23:52:47 +0900

    chore: add tests

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,23 @@
 fn main() {
+    show_openssl_version();
+}
+
+fn show_openssl_version() {
     let version = openssl::version::version();
     println!("OpenSSL version: {}", version);
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::show_openssl_version;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn openssl_version() {
+        show_openssl_version();
+    }
 }
