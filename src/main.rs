9b18776be1965f188e42b1a06b4e1123bb94e543
kitsuyui <kitsuyui@kitsuyui.com>
2023-01-12 21:52:13 +0900

    Use reqwest
    Try to use reqwest. If OpenSSL is linked statically, reqwest should be works properly.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,32 @@
 fn main() {
     show_openssl_version();
+    reqest_to_example_com();
 }
 
 fn show_openssl_version() {
     let version = openssl::version::version();
     println!("OpenSSL version: {}", version);
+}
+
+fn reqest_to_example_com() {
+    let client = reqwest::blocking::Client::new();
+    match client.get("https://example.com").send() {
+        Ok(res) => {
+            println!("Status: {}", res.status());
+            println!("Headers:\n{:#?}", res.headers());
+            match res.text() {
+                Ok(body) => {
+                    println!("Body:\n{}", body);
+                }
+                Err(err) => {
+                    println!("Error: {}", err);
+                }
+            }
+        }
+        Err(err) => {
+            println!("Error: {}", err);
+        }
+    }
 }
 
 #[cfg(test)]
