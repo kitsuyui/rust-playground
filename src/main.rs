2b4bbf57e179f537e462183de32767faed8c197d
kitsuyui <kitsuyui@kitsuyui.com>
2023-01-13 20:17:36 +0900

    Use ring and hex

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
 fn main() {
     show_openssl_version();
     reqest_to_example_com();
+    let digest = get_digest();
+    println!("Digest: {}", digest);
 }
 
 fn show_openssl_version() {
     let version = openssl::version::version();
     println!("OpenSSL version: {}", version);
+}
+
+fn get_digest() -> String {
+    use ring::digest;
+    let bin = digest::digest(&digest::SHA256, b"hello, world");
+    hex::encode(bin)
 }
 
 fn reqest_to_example_com() {
@@ -31,6 +39,7 @@ fn reqest_to_example_com() {
 
 #[cfg(test)]
 mod tests {
+    use super::*;
     use crate::show_openssl_version;
 
     #[test]
@@ -41,5 +50,12 @@ mod tests {
     #[test]
     fn openssl_version() {
         show_openssl_version();
+    }
+
+    #[test]
+    fn test_digest() {
+        let expected_hex = "09ca7e4eaa6e8ae9c7d261167129184883644d07dfba7cbfbc4c8a2e08360d5b";
+        let actual = get_digest();
+        assert_eq!(expected_hex, actual);
     }
 }
