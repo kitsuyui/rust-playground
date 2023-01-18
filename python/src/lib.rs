e7dfda64d00eaed06b0ba37e0f5411d0be5bf502
kitsuyui <kitsuyui@kitsuyui.com>
2023-01-19 00:00:09 +0900

    import amazing_calc

---
+++ python/src/lib.rs
@@ -0,0 +1,14 @@
+use kitsuyui_rust_playground_lib as kitsuyui_rust_playground;
+use pyo3::prelude::*;
+
+#[pyfunction]
+fn my_calc(a: i64, b: i64, c: i64) -> PyResult<String> {
+    Ok(kitsuyui_rust_playground::my_calc(a, b, c))
+}
+
+#[pymodule]
+#[pyo3(name = "kitsuyui_rust_playground")]
+fn python_kitsuyui_rust_playground(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(my_calc, m)?)?;
+    Ok(())
+}
